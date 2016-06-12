825f9b87e7c20d53610781d44934cda7ce496ced
Nicholas Finch <ngf4@nau.edu>
2016-06-11 18:33:33 -0700

    finder program,
    this creates a full list of all directories to searched through later

---
+++ finderUpdate.bat
@@ -0,0 +1,24 @@
+@echo off
+::Update Finder
+::this will be used to update the finder file
+
+::prompt
+:find the data
+::import it into a file
+::exit the program
+
+
+title Finder beta 2.04
+echo -=-Welcome to Finder 1.04-=-
+echo Now updating index file.
+REM clear and refresh the searchlog
+echo. > ".\_searchlog.search"
+
+for /r C:\ %%i in (*.*) do echo ^<a href="%%i"^>%%i ^</a^>^<br^> >> .\_searchlog.search
+for /r E:\ %%i in (*.*) do echo ^<a href="%%i"^>%%i ^</a^>^<br^> >> .\_searchlog.search
+
+del /f/s/q ".\_searchlog(you may delete).search"
+ren ".\_searchlog.search" "_searchlog(you may delete).search"
+echo indexing is finished
+echo exiting in 3 seconds
+timeout /t 3 > nul
